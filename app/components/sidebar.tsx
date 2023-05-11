3b7e1b43d65800ea1fda3056adbe148fd7f9dded
ganweibatao <ganweibatao@tencent.com>
2023-05-11 17:35:35 +0800

    change

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -151,7 +151,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href = “https://github.com/ganweibatao/ChatGPT-Next-Web” target="_blank">
+            <a href={REPO_URL} target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
