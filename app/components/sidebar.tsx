d698ed9b13f79d7c4bd75a5267198429affe8c69
ganweibatao <ganweibatao@tencent.com>
2023-08-16 22:27:19 +0800

    change color and words

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>small.tao`s ChatGPT</div>
+        <div className={styles["sidebar-title"]}>SMALL TAO</div>
         <div className={styles["sidebar-sub-title"]}>
-          small.tao`s AI assistant.
+          Really,really,badly.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
